9886b9777a51ce80683362bf1367e7520f551820
Wenlei Dai <resca.wen@gmail.com>
2019-05-14 14:38:49 +0300

    musicsheet deleted all the useless from textbox

--- src/partials/MusicSheet.js
+++ src/partials/MusicSheet.js
@@ -4,31 +4,6 @@ import { Notation, Midi } from 'react-abc';
 
 import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
 
-const Wrapper = styled.aside`
-  padding 1rem;
-  margin-bottom: 2rem;
-  border-left: 0.2rem solid var(--color);
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-  border-radius: 4px;
-`
-
-const Header = styled.h3`
-  font-size: 1.3rem;
-  font-weight: normal;
-  padding-bottom: 1rem;
-  border-bottom: 1px solid #f7f7f9;
-`
-
-const Body = styled.div`
-  padding-bottom: 0.5rem;
-`
-
-const variantToColor = {
-  hint: "#528afc",
-  learningObjectives: "#57b181",
-  example: "#528afc",
-}
-
 const id = 'a-random-id';
 
 const MusicSheet = props => {
