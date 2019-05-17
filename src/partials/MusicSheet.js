a0e8f89fb10aa75862527cc8d03c20570c511d4b
Wenlei Dai <resca.wen@gmail.com>
2019-05-17 11:31:22 +0300

    note scaling and playback button in correct positions

--- src/partials/MusicSheet.js
+++ src/partials/MusicSheet.js
@@ -1,6 +1,7 @@
 import React from "react"
 import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
 import Fab from "@material-ui/core/Fab"
+import PlayArrowIcon from "@material-ui/icons/PlayArrow"
 
 class MusicSheet extends React.Component {
   constructor(props) {
@@ -32,26 +33,36 @@ class MusicSheet extends React.Component {
       add_classes: false,
       editable: false,
       listener: null,
-      paddingbottom: 30,
+      paddingbottom: 1,
       paddingleft: 50,
       paddingright: 50,
       paddingtop: 15,
       responsive: undefined,
       scale: 3,
-      staffwidth: 740,
+      staffwidth: 400,
     }
 
     return (
-      <div>
-        <react_abc.Notation
-          notation={notation}
-          engraverParams={engraverParams}
-        />
-        {/* edit playback button styling in \node_modules\react-abc\dist\midi\style.css */}
-        <react_abc.Midi notation={notation} />
-        <Fab color="secondary" onClick={this.onPlay}>
-          Soita
-        </Fab>
+      <div class="overall-container">
+        <div class="left-container">
+          <react_abc.Notation
+            notation={notation}
+            engraverParams={engraverParams}
+          />
+          {/* edit playback button styling in \node_modules\react-abc\dist\midi\style.css */}
+          <react_abc.Midi notation={notation} />
+          <div class="playbutton">
+            <Fab color="primary" onClick={this.onPlay}>
+              <PlayArrowIcon />
+            </Fab>
+          </div>
+        </div>
+        <div class="right-container">
+          <react_abc.Notation
+            notation={notation}
+            engraverParams={engraverParams}
+          />
+        </div>
       </div>
     )
   }
