1f43128e40963b7c243a546df7714fc3b75818ec
Rsl1122 <djrapitops@hotmail.com>
2019-05-24 10:36:53 +0300

    Temp

---
+++ src/components/music/react-abc-modified/Midi.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+import renderMidi from "./abc_tunebook_midi"
+import { midiProps } from "./props"
+import "./style.css"
+
+class Midi extends Component {
+  componentDidMount() {
+    const {
+      el,
+      engraverParams,
+      midiParams,
+      notation,
+      parserParams,
+      renderParams,
+    } = this.props
+
+    renderMidi(
+      el || this.el,
+      notation,
+      engraverParams,
+      parserParams,
+      midiParams,
+      renderParams,
+    )
+  }
+
+  render() {
+    return (
+      <div
+        ref={input => {
+          this.el = input
+        }}
+      />
+    )
+  }
+}
+
+Midi.propTypes = {
+  el: PropTypes.node,
+  engraverParams: PropTypes.object,
+  midiParams: PropTypes.object,
+  notation: PropTypes.string.isRequired,
+  parserParams: PropTypes.object,
+  renderParams: PropTypes.object,
+}
+
+Midi.defaultProps = midiProps
+
+export default Midi
