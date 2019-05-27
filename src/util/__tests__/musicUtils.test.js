bb29c9de25e42973fc028e11fe50ec6c62d8a42e
nigosH <32242577+nigoshh@users.noreply.github.com>
2019-05-27 10:55:50 +0300

    Clean for squash commit

--- src/util/__tests__/musicUtils.test.js
+++ src/util/__tests__/musicUtils.test.js
@@ -12,29 +12,6 @@ import {
   musicUtils has been designed precisely to avoid hardcoding all possibilities.
 */
 
-describe("Chords.notation()", () => {
-  it("returns correct G sharp diminished chord", () => {
-    const gSharp = roots[11]
-    const diminishedTriad = triads[0]
-    expect(diminishedTriad.notation(gSharp)).toBe("L:1/1\n[^GBd]")
-  })
-  it("returns correct E flat minor chord", () => {
-    const eFlat = roots[5]
-    const minorTriad = triads[1]
-    expect(minorTriad.notation(eFlat)).toBe("L:1/1\n[_E_G_B]")
-  })
-  it("returns correct A flat major chord", () => {
-    const aFlat = roots[12]
-    const majorTriad = triads[2]
-    expect(majorTriad.notation(aFlat)).toBe("L:1/1\n[_Ac_e]")
-  })
-  it("returns correct B augmented chord", () => {
-    const b = roots[16]
-    const augmentedTriad = triads[3]
-    expect(augmentedTriad.notation(b)).toBe("L:1/1\n[B^d^^f]")
-  })
-})
-
 describe("interval()", () => {
   it("returns D sharp as E's major seventh", () => {
     const e = roots[6]
@@ -117,5 +94,28 @@ describe("interval()", () => {
       for (const quality of qualities)
         for (const number of numbersForQualities[quality.name])
           expect(interval(root, quality.name, number)).toBeTruthy()
+  })
+})
+
+describe("Chords.notation()", () => {
+  it("returns correct G sharp diminished chord", () => {
+    const gSharp = roots[11]
+    const diminishedTriad = triads[0]
+    expect(diminishedTriad.notation(gSharp)).toBe("L:1/1\n[^GBd]")
+  })
+  it("returns correct E flat minor chord", () => {
+    const eFlat = roots[5]
+    const minorTriad = triads[1]
+    expect(minorTriad.notation(eFlat)).toBe("L:1/1\n[_E_G_B]")
+  })
+  it("returns correct A flat major chord", () => {
+    const aFlat = roots[12]
+    const majorTriad = triads[2]
+    expect(majorTriad.notation(aFlat)).toBe("L:1/1\n[_Ac_e]")
+  })
+  it("returns correct B augmented chord", () => {
+    const b = roots[16]
+    const augmentedTriad = triads[3]
+    expect(augmentedTriad.notation(b)).toBe("L:1/1\n[B^d^^f]")
   })
 })
