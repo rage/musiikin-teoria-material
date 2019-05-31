a235cfa9fbf30ef01328816a5b2a7e46070799c8
Rsl1122 <djrapitops@hotmail.com>
2019-05-31 13:49:32 +0300

    Forked react-abc

---
+++ src/components/music/react-abc/index.js
@@ -0,0 +1,7 @@
+import Editor from "./editor"
+import Midi from "./midi"
+import Notation from "./notation"
+
+export { Editor, Midi, Notation }
+
+export default Notation
---
+++ src/components/music/react-abc/notation/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+import abc from "abcjs"
+
+import { notationProps } from "../defaults/props"
+
+class Notation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.notation = null
+    this.renderNotation = this.renderNotation.bind(this)
+  }
+
+  componentDidMount() {
+    this.renderNotation()
+  }
+
+  componentDidUpdate() {
+    this.renderNotation()
+  }
+
+  renderNotation() {
+    const {
+      el,
+      engraverParams,
+      notation,
+      parserParams,
+      renderParams,
+    } = this.props
+
+    this.notation = abc.renderAbc(
+      el || this.el,
+      notation,
+      engraverParams,
+      parserParams,
+      renderParams,
+    )
+  }
+
+  render() {
+    return (
+      <div
+        ref={input => {
+          this.el = input
+        }}
+      />
+    )
+  }
+}
+
+Notation.propTypes = {
+  el: PropTypes.node,
+  engraverParams: PropTypes.object,
+  notation: PropTypes.string.isRequired,
+  parserParams: PropTypes.object,
+  renderParams: PropTypes.object,
+}
+
+Notation.defaultProps = notationProps
+
+export default Notation
