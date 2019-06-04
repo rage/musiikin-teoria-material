76b1e63822d282ea507a78e8791ca3b18bd764cb
Risto Lahtela <djrapitops@hotmail.com>
2019-06-04 11:03:06 +0300

    Refactored ChordExercise into Chord and Exercise (#114)
    ### `Chord`
    
    Chord is a class that can generate chord exercise answers and check that two answers are equal.
    It also manages some exercise specific things, like readable format for the answer and `engraverParams`.
    
    ### `Exercise`
    
    More generic version of `ChordExercise`, now stripped of all logic that ties it to a specific exercise.
    The same file can be used for other exercise kinds once they use same format as `Chord`.
    
    #### answerKeys
    
    JavaScript allows accessing object parameters dynamically with `object["parameterName"]`. This is used a great lot in generalizing the exercise class, as the parameters should not be known by `Exercise`.
    
    This can be seen a lot when generating `selectionOptions` for `SelectionBar` inside `Exercise.render()`

---
+++ src/components/music/Chord.js
@@ -0,0 +1,97 @@
+import {
+  roots as notationRoots,
+  answerOptionsForRoots as answerRoots,
+} from "../../util/music/roots"
+import { triads as answerTriads } from "../../util/music/chords"
+import { randomIntArray } from "../../util/random"
+
+// Answer Keys
+const ROOT = "root",
+  TRIAD = "triad"
+
+/**
+ * Private method, generate a number of correct answers.
+ * @param {*} howMany How many exercises to generate
+ * @returns [{root: 5, triad: 4, notation: "<abc notation>"}]
+ */
+const generateCorrectAnswers = howMany => {
+  const correctRoots = randomIntArray(0, notationRoots.length, howMany)
+  const correctTriads = randomIntArray(0, answerTriads.length, howMany)
+
+  return correctRoots.map((root, i) => {
+    const triad = correctTriads[i]
+    return {
+      root: root,
+      triad: triad,
+      notation: answerTriads[triad].notation(notationRoots[root]),
+    }
+  })
+}
+
+export default class Chord {
+  generateExerciseSet(howMany) {
+    const exerciseSet = {
+      answerKeys: [ROOT, TRIAD],
+      answerOptions: {
+        root: answerRoots,
+        triad: answerTriads,
+      },
+      answerLabels: {
+        root: "Pohjasävel",
+        triad: "Laatu",
+      },
+      exercises: generateCorrectAnswers(howMany),
+    }
+
+    return exerciseSet
+  }
+
+  /**
+   * Get answerKeys that were same in both answer and correctAnswer.
+   * @param {*} answer {root: 5, triad: 3}
+   * @param {*} correctAnswer {root: 5, triad: 2}
+   * @returns ["root"]
+   */
+  getCorrectAnswerKeys(answer, correctAnswer) {
+    if (!answer.root || !correctAnswer) return []
+
+    const correctAnswerKeys = []
+
+    if (answer.root.pitch === correctAnswer.root.pitch)
+      correctAnswerKeys.push(ROOT)
+    if (answer.triad === correctAnswer.triad) correctAnswerKeys.push(TRIAD)
+
+    return correctAnswerKeys
+  }
+
+  /**
+   * Get user readable string of the answer.
+   * @param {*} answer {root: 0, triad: 0}
+   * @returns "C major"
+   */
+  readableAnswerString(answer) {
+    return (
+      notationRoots[answer.root].label +
+      " " +
+      answerTriads[answer.triad].label.toLowerCase()
+    )
+  }
+
+  /**
+   * Get engraverParams for MusicSheet to display this exercise kind.
+   */
+  getEngraverParams() {
+    return {
+      add_classes: false,
+      editable: false,
+      listener: null,
+      paddingbottom: 1,
+      paddingleft: 216,
+      paddingright: 232,
+      paddingtop: 15,
+      responsive: undefined,
+      scale: 3,
+      staffwidth: 300,
+    }
+  }
+}
---
+++ src/components/music/Exercise.js
@@ -0,0 +1,207 @@
+import React from "react"
+import { Paper } from "@material-ui/core"
+import withSimpleErrorBoundary from "../../util/withSimpleErrorBoundary"
+
+import MusicSheet from "../../partials/MusicSheet"
+import CheckAnswerPopper from "./CheckAnswerPopper"
+import SelectionBar from "./SelectionBar"
+import Loading from "../Loading"
+
+class Exercise extends React.Component {
+  state = {
+    render: false,
+
+    // For "wrong answer" popper
+    open: false,
+    placement: undefined,
+
+    // Answer given by student, parts are set in setAnswer.
+    // Contains index numbers.
+    answer: {},
+    // Correct answers generated based on props.exerciseKind
+    exerciseSet: {
+      // Keys that define how the answer, options and correct answers
+      // are formatted, eg. ["root", "triad"]
+      answerKeys: undefined,
+      // All possible options for each answer eg {root: [], triad: []}
+      answerOptions: {},
+      // Correct answers for each exercise
+      exercises: [],
+    },
+    // Current exercise in exerciseSet.exercises
+    currentExerciseIndex: 0,
+
+    // When the user clicks "Send answer"
+    answerWasSubmitted: false,
+  }
+
+  componentDidMount() {
+    this.setState({ render: true })
+    this.nextExerciseSet()
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
+  /**
+   * Method to call when submit answer button is pressed.
+   * @returns false if not all answers are selected, true if the answer was submitted
+   */
+  handleClick = placement => event => {
+    if (!this.isAnswerSet()) {
+      return false
+    }
+    const { currentTarget } = event
+    if (this.isAnswerCorrect()) {
+      this.props.onCorrect()
+      this.nextExercise()
+    } else {
+      this.props.onIncorrect()
+      this.setState(state => ({
+        anchorEl: currentTarget,
+        open: state.placement !== placement || !state.open,
+        placement,
+        answerWasSubmitted: true,
+      }))
+    }
+    return true
+  }
+
+  /**
+   * Check that all needed answers are set in this.state.answer
+   */
+  isAnswerSet = () => {
+    return (
+      this.state.exerciseSet.answerKeys.length ===
+      Object.keys(this.state.answer).length
+    )
+  }
+
+  /**
+   * Check that the given answer is correct
+   */
+  isAnswerCorrect = () => {
+    const answer = this.state.answer
+    const correctAnswer = this.state.exerciseSet.exercises[
+      this.state.currentExerciseIndex
+    ]
+
+    const answerKeysThatAreCorrect = this.props.exerciseKind.getCorrectAnswerKeys(
+      answer,
+      correctAnswer,
+    )
+
+    return this.state.exerciseSet.answerKeys.every(answer =>
+      answerKeysThatAreCorrect.includes(answer),
+    )
+  }
+
+  /**
+   * Function to create a setAnswer function for a dropdown
+   * @param {*} answerOption Key in this.state.exerciseSet.answerKeys
+   * @returns A function that sets given answer to the correct key in this.state.answer
+   */
+  setAnswer = answerOption => studentAnswer => {
+    const answer = this.state.answer
+    answer[answerOption] = studentAnswer
+    this.setState({ answer })
+  }
+
+  /**
+   * Generate new set of exercises and reset answer
+   */
+  nextExerciseSet = () => {
+    this.setState({
+      currentExerciseIndex: 0,
+      exerciseSet: this.props.exerciseKind.generateExerciseSet(
+        this.props.requiredAnswers,
+      ),
+      render: true,
+      open: false,
+      answerWasSubmitted: false,
+      answer: {},
+    })
+  }
+
+  /**
+   * Take next exercise from the exercise set and reset answer
+   *
+   * Duplicate code can not be removed (Similar above), because
+   * multiple setState calls within same method cause overrides
+   */
+  nextExercise = () => {
+    this.setState({
+      currentExerciseIndex: this.state.currentExerciseIndex + 1,
+      open: false,
+      answerWasSubmitted: false,
+      answer: {},
+    })
+  }
+
+  render() {
+    if (!this.state.render) {
+      return null
+    }
+
+    const exerciseSet = this.state.exerciseSet
+    const correctAnswer = exerciseSet.exercises[this.state.currentExerciseIndex]
+    // answerKeys that were same in answer and correctAnswer
+    const answerKeysThatAreCorrect = this.props.exerciseKind.getCorrectAnswerKeys(
+      this.state.answer,
+      correctAnswer,
+    )
+
+    const selectionOptions = exerciseSet.answerKeys.map(key => {
+      const answerOptions = exerciseSet.answerOptions[key]
+
+      return {
+        setAnswer: this.setAnswer(key),
+        answers: answerOptions,
+        label: exerciseSet.answerLabels[key],
+        selectedIndex: this.state.answer[key],
+        borderColor: this.state.answerWasSubmitted
+          ? answerKeysThatAreCorrect.includes(key)
+            ? "green"
+            : "red"
+          : "",
+      }
+    })
+
+    return (
+      <Loading loading={!this.state.render}>
+        <CheckAnswerPopper
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          placement={this.state.placement}
+          correctAnswer={
+            // pass correct answer only after the answer was sent; otherwise the
+            // student could read the correct answer using React Developer Tools
+            this.state.answerWasSubmitted
+              ? this.props.exerciseKind.readableAnswerString(correctAnswer)
+              : ""
+          }
+        />
+        <Paper>
+          <div className="overall-container">
+            <MusicSheet
+              notation={correctAnswer.notation}
+              onlynotes={this.props.onlyNotes}
+              onlysound={this.props.onlySound}
+              engraverParams={this.props.exerciseKind.getEngraverParams()}
+              playbuttonstyle={"playButton"}
+            />
+            <SelectionBar
+              options={selectionOptions}
+              answerWasWrong={this.state.answerWasSubmitted}
+              nextExerciseSet={this.nextExerciseSet}
+              handleClick={this.handleClick("top")}
+            />
+          </div>
+        </Paper>
+      </Loading>
+    )
+  }
+}
+
+export default withSimpleErrorBoundary(Exercise)
