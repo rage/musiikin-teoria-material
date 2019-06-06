cd741b646d49a6f338d68f195e7b28dca5a4dec8
Rsl1122 <djrapitops@hotmail.com>
2019-06-06 10:52:41 +0300

    Refactored MusicExercise to use less copy-paste

--- src/partials/MusicExercises.js
+++ src/partials/MusicExercises.js
@@ -7,8 +7,6 @@ import Chord from "../components/music/Chord"
 import Interval from "../components/music/Interval"
 import Scale from "../components/music/Scale"
 
-import { scales, modes } from "../util/music/scales"
-
 /**
  * In charge of routing user exercise type to implementing exercise
  * eg <music-exercise type="chords"> to ChordsExercise
@@ -55,140 +53,55 @@ const getQuizId = type => {
   }
 }
 
+const getExerciseKindByType = type => {
+  if (type.includes("chord")) {
+    return new Chord()
+  } else if (type.includes("interval")) {
+    return new Interval()
+  } else if (type.includes("scale")) {
+    return new Scale("scales")
+  } else if (type.includes("mode")) {
+    return new Scale("modes")
+  } else {
+    return undefined
+  }
+}
+
 const createExerciseRenderingFunction = (type, requiredAnswers) => (
   onCorrectAnswer,
   onIncorrectAnswer,
 ) => {
-  switch (type ? type : "chords") {
-    case "chords":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          exerciseKind={new Chord()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "chords_notes":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlyNotes={true}
-          exerciseKind={new Chord()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "chords_sound":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlySound={true}
-          exerciseKind={new Chord()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "intervals":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          exerciseKind={new Interval()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "intervals_notes":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlyNotes={true}
-          exerciseKind={new Interval()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "intervals_sound":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlySound={true}
-          exerciseKind={new Interval()}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "scales":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          exerciseKind={new Scale(scales)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "scales_notes":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlyNotes={true}
-          exerciseKind={new Scale(scales)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "scales_sound":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlySound={true}
-          exerciseKind={new Scale(scales)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "modes":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          exerciseKind={new Scale(modes)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "modes_notes":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlyNotes={true}
-          exerciseKind={new Scale(modes)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    case "modes_sound":
-      return (
-        <Exercise
-          onCorrect={onCorrectAnswer}
-          onIncorrect={onIncorrectAnswer}
-          onlySound={true}
-          exerciseKind={new Scale(modes)}
-          requiredAnswers={requiredAnswers}
-        />
-      )
-    default:
-      return (
-        <>
-          <p>Incorrect exercise type {"'" + type + "'"}, implemented types:</p>
-          <ul>
-            <li>chords, chords_notes, chords_sound</li>
-            <li>intervals, intervals_notes, intervals_sound</li>
-            <li>scales, scales_notes, scales_sound</li>
-            <li>modes, modes_notes, modes_sound</li>
-          </ul>
-        </>
-      )
+  const wantedType = type ? type : "chords"
+
+  const exerciseKind = getExerciseKindByType(wantedType)
+
+  if (!exerciseKind) {
+    return (
+      <>
+        <p>Incorrect exercise type {"'" + type + "'"}, implemented types:</p>
+        <ul>
+          <li>chords, chords_notes, chords_sound</li>
+          <li>intervals, intervals_notes, intervals_sound</li>
+          <li>scales, scales_notes, scales_sound</li>
+          <li>modes, modes_notes, modes_sound</li>
+        </ul>
+      </>
+    )
   }
+
+  const onlyNotes = wantedType.includes("notes")
+  const onlySound = wantedType.includes("sound")
+
+  return (
+    <Exercise
+      onCorrect={onCorrectAnswer}
+      onIncorrect={onIncorrectAnswer}
+      onlyNotes={onlyNotes}
+      onlySound={onlySound}
+      exerciseKind={exerciseKind}
+      requiredAnswers={requiredAnswers}
+    />
+  )
 }
 
 export default ExerciseRouter
