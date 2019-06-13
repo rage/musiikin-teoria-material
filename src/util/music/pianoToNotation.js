7fa45330932ba13595ebbe06effa5c7b3b0b1284
Vanamo Piirainen <vanamo.piirainen@helsinki.fi>
2019-06-13 14:34:59 +0300

    Add function to convert midi number to pitch

--- src/util/music/pianoToNotation.js
+++ src/util/music/pianoToNotation.js
@@ -1,7 +1,20 @@
 import { notes } from "./roots"
 
-export const getNoteFromPitch = pitch => {
+export const convertMidiNumberToNote = midiNumber => {
+  const pitch = convertMidiNumberToPitch(midiNumber)
   const noteOptions = notes[pitch]
   //return first option for now
   return noteOptions[0]
+}
+
+const convertMidiNumberToPitch = midiNumber => {
+  //midi number 60 = middle c
+  let pitch = midiNumber - 60
+  while (pitch > 11) {
+    pitch = pitch - 12
+  }
+  while (pitch < 0) {
+    pitch = pitch + 12
+  }
+  return pitch
 }
