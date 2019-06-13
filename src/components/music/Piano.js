205f6537a0ed581c33b7ac98905c5054182811cc
nigosH <32242577+nigoshh@users.noreply.github.com>
2019-06-13 15:29:57 +0300

    Fix again window undefined in Now

--- src/components/music/Piano.js
+++ src/components/music/Piano.js
@@ -25,9 +25,6 @@ class Provider extends React.Component {
 
 const DimensionsProvider = Dimensions()(Provider)
 
-// webkitAudioContext fallback needed to support Safari
-const audioContext = new (window.AudioContext || window.webkitAudioContext)()
-
 const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net"
 
 const noteRange = {
@@ -41,28 +38,44 @@ const keyboardShortcuts = KeyboardShortcuts.create({
   keyboardConfig: KeyboardShortcuts.HOME_ROW,
 })
 
-const Piano = props => {
-  return (
-    <DimensionsProvider>
-      {({ containerWidth, containerHeight }) => (
-        <SoundfontProvider
-          hostname={soundfontHostname}
-          audioContext={audioContext}
-          render={({ isLoading, playNote, stopNote }) => (
-            <ReactPiano
-              noteRange={noteRange}
-              width={containerWidth}
-              playNote={playNote}
-              stopNote={stopNote}
-              disabled={isLoading}
-              keyboardShortcuts={keyboardShortcuts}
-              {...props}
-            />
-          )}
-        />
-      )}
-    </DimensionsProvider>
-  )
+class Piano extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      audioContext: undefined,
+    }
+  }
+
+  componentDidMount() {
+    // webkitAudioContext fallback needed to support Safari
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)()
+    this.setState({ audioContext })
+  }
+
+  render() {
+    return (
+      <DimensionsProvider>
+        {({ containerWidth, containerHeight }) => (
+          <SoundfontProvider
+            hostname={soundfontHostname}
+            audioContext={this.state.audioContext}
+            render={({ isLoading, playNote, stopNote }) => (
+              <ReactPiano
+                noteRange={noteRange}
+                width={containerWidth}
+                playNote={playNote}
+                stopNote={stopNote}
+                disabled={isLoading}
+                keyboardShortcuts={keyboardShortcuts}
+                {...this.props}
+              />
+            )}
+          />
+        )}
+      </DimensionsProvider>
+    )
+  }
 }
 
 export default Piano
