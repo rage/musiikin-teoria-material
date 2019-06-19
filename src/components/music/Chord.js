75b83a9264aabad67fb3cf4b6226f61bb63486fd
Rsl1122 <djrapitops@hotmail.com>
2019-06-19 14:56:06 +0300

    Requested changes

--- src/components/music/Chord.js
+++ src/components/music/Chord.js
@@ -164,7 +164,7 @@ export default class Chord {
    */
   shouldAddNote = (note, notes) => {
     return (
-      notes.length <= this.getNoteLimits().max &&
+      notes.length < this.getNoteLimits().max &&
       !notes.map(n => n.notation).includes(note.notation)
     )
   }
